763f0df44c6a3ca764d2c266cd782758ecec34ff
Namratha <namratha.mraj@gmail.com>
2018-01-31 19:56:55 +0530

    Add files via upload

--- test.c
+++ test.c
@@ -6,18 +6,6 @@
 
 float X1, Y1, X2, Y2;
 
-void bresenham();
-
-void bresenham()
-{
-}#include<GL/glut.h>
-#include<GL/gl.h>
-#include<GL/glu.h>
-#include<math.h>
-#include<stdio.h>
-
-float X1, Y1, X2, Y2;
-
 float round_value(float v)
 {
   return floor(v + 0.5);
@@ -80,34 +68,5 @@ void main(int argc, char **argv)
   init();
   glutDisplayFunc(DDAline);
   glutMainLoop();
-}
-
-
-void init()
-{
-	
-	glClearColor(1,1,1,1);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0,500,0,500);
-	
-}
-
-void main(int argc, char**argv)
-{
-	/*printf("Enter two end points of the line to be drawn:\n");
-	printf("\nEnter Point1( X1 , Y1):\n");
-	scanf("%f%f",&X1,&Y1);
-	printf("\nEnter Point1( X2 , Y2):\n");
-	scanf("%f%f",&X2,&Y2);*/
-
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowPosition(0,0);
-  	glutInitWindowSize(500,500);
-	glutCreateWindow("Drawing");
-	init();
-	glutDisplayFunc(bresenham);
-	glutMainLoop();
 }
 
