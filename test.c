cc7e4aec324328830225e42e73057d3574fff6f3
Namratha <namratha.mraj@gmail.com>
2018-01-31 19:57:27 +0530

    Add files via upload

--- test.c
+++ test.c
@@ -6,67 +6,68 @@
 
 float X1, Y1, X2, Y2;
 
-float round_value(float v)
-{
-  return floor(v + 0.5);
-}
-
-void DDAline(void)
-{
-  float dx=(X2-X1);
-  float dy=(Y2-Y1);
-  float steps;
-  float xInc,yInc,x=X1,y=Y1;
-  /* Find out whether to increment x or y */
-  steps=(abs(dx)>abs(dy))?(abs(dx)):(abs(dy));
-  xInc=dx/(float)steps;
-  yInc=dy/(float)steps;
-
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  glColor3f(0,0,0);
-  glBegin(GL_LINES);
-  glPointSize(5.0);
-  //glVertex2d(x,y);
-  int k;
- 
-  for(k=0;k<steps;k++)
-  {
-    glVertex2d(round_value(x), round_value(y));
-    x+=xInc;
-    y+=yInc;
-    //printf("%f,  %f\n",x,y);
-    glVertex2d(round_value(x), round_value(y));
-  }
-  glEnd();
-
-  glFlush();
-}
-
 void init()
 {
+	
 	glClearColor(1,1,1,1);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0,500,0,500);
+	//printf("hello");
 }
 
 
-void main(int argc, char **argv)
+//Bresenham line-drawing procedure for |m| < 1.0
+/*void bresenham()
 {
-  printf("Enter two end points of the line to be drawn:\n");
-  printf("\nEnter Point1( X1 , Y1):\n");
-  scanf("%f%f",&X1,&Y1);
-  printf("\nEnter Point1( X2 , Y2):\n");
-  scanf("%f%f",&X2,&Y2);
-  
-  glutInit(&argc,argv);
-  glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-  glutInitWindowPosition(0,0);
-  glutInitWindowSize(500,500);
-  glutCreateWindow("DDA_Line");
-  init();
-  glutDisplayFunc(DDAline);
-  glutMainLoop();
+ GLint dx = abs(X2-X1);
+ GLint dy = abs(Y2-Y1);
+ GLint p = 2*dy - dx;
+ GLint twoDy = 2*dy;
+ GLint twoDyMinusDx = 2*(dy-dx);
+ GLint x,y;
+ // determine which endpoint to use as start position
+ if (X1>X2){
+	 x=X2;
+	 y=Y2;
+	 X2=x;
+ }else{
+	 x=X1; 
+	 y=X1;
+ }
+ glClear(GL_COLOR_BUFFER_BIT);
+ glColor3f(0,0,0);
+ glBegin(GL_LINES);
+ while(x<X2){
+
+	 glVertex2f(x,y); 
+	 x++;
+	 if(p<0)
+	 p+=twoDy;
+	 else{
+	 y++;
+	 p+=twoDyMinusDx;
+ 	}
+	glVertex2f(x,y); 
+ }
+ glEnd();
+ glFlush();
+} */ 
+
+void main(int argc, char**argv)
+{
+	/*printf("Enter two end points of the line to be drawn:\n");
+	printf("\nEnter Point1( X1 , Y1):\n");
+	scanf("%f%f",&X1,&Y1);
+	printf("\nEnter Point1( X2 , Y2):\n");
+	scanf("%f%f",&X2,&Y2);*/
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowPosition(0,0);
+  	glutInitWindowSize(500,500);
+	glutCreateWindow("Drawing");
+	init();
+	//glutDisplayFunc(bresenham);
+	glutMainLoop();
 }
 
